4ef7303c4a40c953272679e257428742f4c2d3a7
LuisaFC <luisabsb@gmail.com>
2025-04-03 16:09:57 -0300

    feat: back to home button

--- src/components/Analytics/AnalyticsHeader.tsx
+++ src/components/Analytics/AnalyticsHeader.tsx
@@ -1,15 +1,27 @@
 import { Button } from '@/components/ui/button';
 import { AnalyticsHeaderProps } from '@/types/analytics';
-import { RefreshCw } from 'lucide-react';
+import { ArrowLeft, RefreshCw } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export function AnalyticsHeader({ onRefresh }: AnalyticsHeaderProps) {
+  const router = useRouter();
+
   return (
     <header className="flex items-center justify-between">
-      <div>
-        <h1 className="text-3xl font-bold">Painel de Controle</h1>
-        <p className="text-muted-foreground mt-1">
-          Visualize as estatísticas das suas tarefas
-        </p>
+      <div className="flex items-center gap-4">
+      <Button
+          variant="ghost"
+          onClick={() => router.push('/')}
+          className="!h-10 !w-10 hover:bg-muted mr-1" // Forçando tamanho com !important
+        >
+          <ArrowLeft className="!h-6 !w-6" /> {/* Forçando tamanho com !important */}
+        </Button>
+        <div>
+          <h1 className="text-3xl font-bold">Painel de Controle</h1>
+          <p className="text-muted-foreground mt-1">
+            Visualize as estatísticas das suas tarefas
+          </p>
+        </div>
       </div>
       <Button
         variant="outline"
