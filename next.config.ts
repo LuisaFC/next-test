c7041741c94f6684981f30d5032be56a478e0e5f
LuisaFC <luisabsb@gmail.com>
2025-04-04 11:13:41 -0300

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: [],
-  },
-  i18n: {
-    locales: ['en', 'pt'],
-    defaultLocale: 'en',
   },
 };
 
